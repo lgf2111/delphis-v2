8d584a7150a4c467566f03f25d71966bed419b88
Jeff Lee <lgf2111@gmail.com>
2024-03-27 20:22:46 +0800

    second draft search

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,8 @@
 import React, { useRef, useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
+import { FaCircleDollarToSlot } from "react-icons/fa6";
+import { IoIosCloseCircle } from "react-icons/io";
+import { MdLocationOn } from "react-icons/md";
 
 export default function Search() {
   return (
@@ -10,62 +14,94 @@ export default function Search() {
 }
 
 function Filters() {
+  const [filters, setFilters] = useState<string[]>([]);
+
+  const onClick = (newFilter: string) => {
+    if (!filters.includes(newFilter)) {
+      setFilters([...filters, newFilter]);
+    }
+    setFilters((filters) => filters.filter((item) => item !== newFilter));
+  };
+
+  function Card({ filter }: { filter: { title: string; items: string[] } }) {
+    return (
+      <div>
+        <h3 className="pb-2 text-lg font-bold">{filter.title}</h3>
+        <div className="flex flex-wrap gap-1">
+          {filter.items.map((item, index) => (
+            <button
+              key={index}
+              className={`btn btn-sm ${filters.includes(item) && "btn-secondary"}`}
+              onClick={() => onClick(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   function Subject() {
     const modalRef = useRef<HTMLDialogElement | null>(null);
+    const subjects = [
+      {
+        title: "O & N Level",
+        items: [
+          "English",
+          "Mother Tongue",
+          "H. Mother Tongue",
+          "Mother Tongue B",
+          "Third Language",
+          "E. Maths",
+          "A. Maths",
+          "POA",
+          "P. Biology",
+          "P. Physics",
+          "P. Chemistry",
+          "CS (Chem/Bio)",
+          "CS (Chem/Phy)",
+          "CS (Bio/Phy)",
+          "P. Literature",
+          "P. Geography",
+          "P. History",
+          "CH (SS/Lit)",
+          "CH (SS/His)",
+          "CH (SS/Lit in MT)",
+          "D&T",
+          "F&N",
+          "Art",
+          "Music",
+          "H. Art",
+          "H. Music",
+          "Drama",
+          "Computing",
+          "Electronics",
+        ],
+      },
+      {
+        title: "PSLE",
+        items: [
+          "English",
+          "Mother Tongue",
+          "H. Mother Tongue",
+          "Maths",
+          "Science",
+          "Social Studies",
+        ],
+      },
+    ];
 
     return (
       <>
         <button className="btn" onClick={() => modalRef.current?.showModal()}>
-          Subject
+          Subject <FaCaretDown />
         </button>
         <dialog ref={modalRef} className="modal">
           <div className="modal-box flex flex-col gap-5">
-            {/* <h3 className="text-lg font-bold">A Level</h3> */}
-            <div className="">
-              <h3 className="pb-2 text-lg font-bold">O & N Level</h3>
-              <div className="flex flex-wrap gap-1">
-                <button className="btn btn-sm">English</button>
-                <button className="btn btn-sm">Mother Tongue</button>
-                <button className="btn btn-sm">H. Mother Tongue</button>
-                <button className="btn btn-sm">Mother Tongue B</button>
-                <button className="btn btn-sm">Third Language</button>
-                <button className="btn btn-sm">E. Maths</button>
-                <button className="btn btn-sm">A. Maths</button>
-                <button className="btn btn-sm">POA</button>
-                <button className="btn btn-sm">P. Biology</button>
-                <button className="btn btn-sm">P. Physics</button>
-                <button className="btn btn-sm">P. Chemistry</button>
-                <button className="btn btn-sm">CS (Chem/Bio)</button>
-                <button className="btn btn-sm">CS (Chem/Phy)</button>
-                <button className="btn btn-sm">CS (Bio/Phy)</button>
-                <button className="btn btn-sm">P. Literature</button>
-                <button className="btn btn-sm">P. Geography</button>
-                <button className="btn btn-sm">P. History</button>
-                <button className="btn btn-sm">CH (SS/Lit)</button>
-                <button className="btn btn-sm">CH (SS/His)</button>
-                <button className="btn btn-sm">CH (SS/Lit in MT)</button>
-                <button className="btn btn-sm">D&T</button>
-                <button className="btn btn-sm">F&N</button>
-                <button className="btn btn-sm">Art</button>
-                <button className="btn btn-sm">Music</button>
-                <button className="btn btn-sm">H. Art</button>
-                <button className="btn btn-sm">H. Music</button>
-                <button className="btn btn-sm">Drama</button>
-                <button className="btn btn-sm">Computing</button>
-                <button className="btn btn-sm">Electronics</button>
-              </div>
-            </div>
-            <div className="">
-              <h3 className="pb-2 text-lg font-bold">PSLE</h3>
-              <div className="flex flex-wrap gap-1">
-                <button className="btn btn-sm">English</button>
-                <button className="btn btn-sm">Mother Tongue</button>
-                <button className="btn btn-sm">H. Mother Tongue</button>
-                <button className="btn btn-sm">Maths</button>
-                <button className="btn btn-sm">Science</button>
-                <button className="btn btn-sm">Social Studies</button>
-              </div>
-            </div>
+            {subjects.map((subject, index) => (
+              <Card filter={subject} key={index} />
+            ))}
           </div>
           <form method="dialog" className="modal-backdrop">
             <button>close</button>
@@ -79,8 +115,12 @@ function Filters() {
     const modalRef = useRef<HTMLDialogElement | null>(null);
     return (
       <>
-        <button className="btn" onClick={() => modalRef.current?.showModal()}>
-          Location
+        <button
+          className="btn"
+          onClick={() => modalRef.current?.showModal()}
+          disabled
+        >
+          Location <FaCaretDown />
         </button>
         <dialog ref={modalRef} className="modal">
           <div className="modal-box">
@@ -96,34 +136,20 @@ function Filters() {
   }
   function Grade() {
     const modalRef = useRef<HTMLDialogElement | null>(null);
+    const grades = [
+      { title: "Secondary School", items: ["S1", "S2", "S3", "S4", "S5"] },
+      { title: "Primary School", items: ["P1", "P2", "P3", "P4", "P5", "P6"] },
+    ];
     return (
       <>
         <button className="btn" onClick={() => modalRef.current?.showModal()}>
-          Grade
+          Grade <FaCaretDown />
         </button>
         <dialog ref={modalRef} className="modal">
           <div className="modal-box flex flex-col gap-5">
-            <div className="">
-              <h3 className="pb-2 text-lg font-bold">Secondary School</h3>
-              <div className="flex flex-wrap gap-1">
-                <button className="btn btn-sm">S1</button>
-                <button className="btn btn-sm">S2</button>
-                <button className="btn btn-sm">S3</button>
-                <button className="btn btn-sm">S4</button>
-                <button className="btn btn-sm">S5</button>
-              </div>
-            </div>
-            <div className="">
-              <h3 className="pb-2 pt-5 text-lg font-bold">Primary School</h3>
-              <div className="flex flex-wrap gap-1">
-                <button className="btn btn-sm">P1</button>
-                <button className="btn btn-sm">P2</button>
-                <button className="btn btn-sm">P3</button>
-                <button className="btn btn-sm">P4</button>
-                <button className="btn btn-sm">P5</button>
-                <button className="btn btn-sm">P6</button>
-              </div>
-            </div>
+            {grades.map((grade, index) => (
+              <Card filter={grade} key={index} />
+            ))}
           </div>
           <form method="dialog" className="modal-backdrop">
             <button>close</button>
@@ -138,8 +164,12 @@ function Filters() {
 
     return (
       <>
-        <button className="btn" onClick={() => modalRef.current?.showModal()}>
-          Others
+        <button
+          className="btn"
+          onClick={() => modalRef.current?.showModal()}
+          disabled
+        >
+          Others <FaCaretDown />
         </button>
         <dialog ref={modalRef} className="modal">
           <div className="modal-box">
@@ -154,10 +184,26 @@ function Filters() {
   }
   return (
     <>
-      <Subject />
-      <Location />
-      <Grade />
-      <Others />
+      <div className="flex justify-center gap-4">
+        <Subject />
+        <Location />
+        <Grade />
+        <Others />
+      </div>
+      <div className="flex justify-center gap-2 py-5">
+        {filters.map((filter, index) => (
+          <button
+            key={index}
+            className="badge badge-outline badge-lg"
+            onClick={() =>
+              setFilters((filters) => filters.filter((item) => item !== filter))
+            }
+          >
+            {filter}&nbsp;
+            <IoIosCloseCircle />
+          </button>
+        ))}
+      </div>
     </>
   );
 }
@@ -166,14 +212,14 @@ function Tutors() {
   function TutorCard() {
     const [tab, setTab] = useState("Profile");
     return (
-      <div className="card bg-base-100 w-1/4 shadow-xl">
+      <div className="card bg-base-100 w-1/2 shadow-xl md:w-1/3 lg:w-1/4">
         <figure>
           <img
             src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
             alt="Shoes"
           />
         </figure>
-        <div className="card-body">
+        <div className="card-body overflow-hidden">
           <div role="tablist" className="tabs tabs-bordered">
             <button
               role="tab"
@@ -201,15 +247,34 @@ function Tutors() {
           {tab === "Profile" && <p>General Paper, Maths, Physics</p>}
           {tab === "Intro" && <p>Degree in NUS</p>}
           {tab === "Timetable" && <p>...</p>}
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="flex pt-5">
+            <div className="flex-1">
+              <div className="flex gap-1">
+                <MdLocationOn />
+                <span>
+                  -
+                  <br />
+                  Location
+                </span>
+              </div>
+            </div>
+            <div className="flex-1">
+              <div className="flex gap-1">
+                <FaCircleDollarToSlot />
+                <span>
+                  SGD $10 Up
+                  <br />
+                  /hour
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
   return (
-    <>
+    <div className="pt-5">
       <h1 className="text-lg font-bold">Find the best tutor for you</h1>
       <small>266 Tutors</small>
       <div className="flex flex-wrap gap-3">
@@ -218,6 +283,6 @@ function Tutors() {
         <TutorCard />
         <TutorCard />
       </div>
-    </>
+    </div>
   );
 }
