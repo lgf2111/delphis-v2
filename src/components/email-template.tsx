593bd941fb7cea1d2ed19d1d229931e0a2317703
Jeff Lee <lgf2111@gmail.com>
2024-04-18 00:51:25 +0800

    send client email when done booking

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -13,36 +13,54 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   </div>
 );
 
-type BookTutorTemplateProps = RouterInputs["email"]["bookTutor"];
-export const BookTutorTemplate: React.FC<Readonly<BookTutorTemplateProps>> = ({
-  name,
-  subject,
-  time,
-  location,
-  duration,
-  message,
-}) => (
-  <div>
-    <header>
-      Hi {name},
-      <br />
-      Here&lsquo;s a new tutor assignment request just for you!
-    </header>
-    <main>
-      <p>Subject: {subject}</p>
-      <p>Time: {time}</p>
-      <p>Location: {location}</p>
-      <p>Duration: {duration}</p>
-      <p>Message: {message}</p>
-    </main>
+type BookingTutorTemplateProps = RouterInputs["email"]["bookTutor"] & {
+  to: "client" | "tutor";
+};
+export const BookingTutorTemplate: React.FC<
+  Readonly<BookingTutorTemplateProps>
+> = ({ to, name, clientName, subject, time, location, duration, message }) => {
+  return (
+    <div>
+      <header>
+        Hi {to === "tutor" ? name : clientName},
+        <br />
+        {to === "tutor" ? (
+          <>Here&lsquo;s a new tutor assignment request just for you!</>
+        ) : (
+          <>Here&lsquo;s the assignment request we have sent to the tutor.</>
+        )}
+      </header>
+      <main>
+        <p>Subject: {subject}</p>
+        <p>Time: {time}</p>
+        <p>Location: {location}</p>
+        <p>Duration: {duration}</p>
+        <p>Message: {message}</p>
+        {to === "tutor" ? (
+          <>
+            Please login to your account to accept or reject the request.
+            <br />
+            With you all the best!
+          </>
+        ) : (
+          <>
+            Please login to your account to view the request status.
+            <br />
+            With you all the best!
+          </>
+        )}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+function Footer() {
+  return (
     <footer>
-      Please login to your account to accept or reject the request.
-      <br />
-      With you all the best!
-      <br />
       Cheryl
       <br />
       Delphis (www.delphis.com)
     </footer>
-  </div>
-);
+  );
+}
--- src/server/api/routers/email.ts
+++ src/server/api/routers/email.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { BookTutorTemplate } from '~/components/email-template';
+import { BookingTutorTemplate } from '~/components/email-template';
 import { Resend } from 'resend';
 import { env } from "~/env";
 import { TRPCError } from "@trpc/server";
@@ -9,18 +9,26 @@ const resend = new Resend(env.RESEND_API_KEY);
 
 export const emailRouter = createTRPCRouter({
     bookTutor: publicProcedure.input(z.object({ clientName: z.string(), clientEmail: z.string().email(), email: z.string().email(), name: z.string(), subject: z.string(), location: z.string(), time: z.string(), duration: z.number(), message: z.string() })).mutation(async ({ ctx, input }) => {
-        const { data, error } = await resend.emails.send({
+        const tutorRes = await resend.emails.send({
             from: 'Cheryl Ong <cheryl.ong@delphistech.com>',
             to: [`${input.name} <${input.email}>`],
             subject: `New Assignment Request`,
             text: "",
-            react: BookTutorTemplate({ ...input }),
+            react: BookingTutorTemplate({ to: "tutor", ...input }),
         });
 
-        if (error) {
+        const clientRes = await resend.emails.send({
+            from: 'Cheryl Ong <cheryl.ong@delphistech.com>',
+            to: [`${input.clientName} <${input.clientEmail}>`],
+            subject: `New Assignment Request`,
+            text: "",
+            react: BookingTutorTemplate({ to: "client", ...input }),
+        });
+
+        if (tutorRes.error ?? clientRes.error) {
             throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" })
         }
 
-        return data;
+        return [tutorRes.data, clientRes.data];
     }),
 });
